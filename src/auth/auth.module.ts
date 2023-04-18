5d6770b9410a3b28fd3d8e8b78ae50f0356a0048
Bastien Aumeunier <bastien.aumeunier@ynov.com>
2023-04-18 10:45:15 +0200

    set JWT to 7d

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -10,7 +10,7 @@ import { JwtStrategy } from './strategies/jwt.strategies';
     imports: [forwardRef(() => UserModule), PassportModule,
       JwtModule.register({
         secret: jwtConstants.secret,
-        signOptions: { expiresIn: '1h' },
+        signOptions: { expiresIn: '7d' },
       }),
     ],
     providers: [AuthService, JwtStrategy],
